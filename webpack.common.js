f4e82790fe1b65e338bebcbc8e188bf585fa036f
Paul John <butadpj@gmail.com>
2022-03-15 02:36:49 +0800

    First commit - Setup webpack (dev & prod) && Finish hero section

---
+++ webpack.common.js
@@ -0,0 +1,15 @@
+const path = require('path');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+
+module.exports = {
+  entry: './src/js/app.js',
+  output: {
+    path: path.resolve(__dirname, 'build/'),
+    filename: 'bundle.js',
+  },
+  plugins: [
+    new CopyWebpackPlugin({
+      patterns: [{ from: './public' }],
+    }),
+  ],
+};
