82f74659279f775063ec51ecb7a1d628d722102b
Paul John <butadpj@gmail.com>
2022-03-15 17:37:53 +0800

    Modularize javascript files && Add script for collapsible elements

---
+++ src/js/parallax.js
@@ -0,0 +1,37 @@
+const parallaxElements = document.querySelectorAll('.parallax');
+const parallaxOpacityElements = document.querySelectorAll('.parallax-opacity');
+
+window.addEventListener('scroll', () => {
+  const scrolled = window.pageYOffset;
+
+  if (parallaxElements.length) {
+    parallaxElements.forEach((element) => {
+      const rate = element.dataset.rate;
+
+      if (rate) {
+        const pos = scrolled * (element.dataset.rate / 10);
+        element.style.transform = `translate3d(0px, ${pos}px, 0px)`;
+        return;
+      }
+
+      element.style.transform = `translate3d(0px, ${scrolled * -0.3}px, 0px)`;
+    });
+  }
+
+  if (parallaxOpacityElements.length) {
+    parallaxOpacityElements.forEach((element) => {
+      const offset = element.offsetTop - 110;
+      const opacity = (scrolled / offset - 1) * -1;
+      element.style.opacity = opacity;
+
+      const rate = element.dataset.rate;
+      if (rate) {
+        const pos = scrolled * (element.dataset.rate / 10);
+        element.style.transform = `translate3d(0px, ${pos}px, 0px)`;
+        return;
+      }
+
+      element.style.transform = `translate3d(0px, ${scrolled * -0.3}px, 0px)`;
+    });
+  }
+});
