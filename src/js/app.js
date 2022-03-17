363ace6ae7151d3f7c8fa380765b1fc305fcd6ac
Paul John <butadpj@gmail.com>
2022-03-17 20:10:21 +0800

    Fix nav content height issue on mobile devices

--- src/js/app.js
+++ src/js/app.js
@@ -7,15 +7,3 @@ lib.butter.init({
   cancelOnTouch: true,
   wrapperDamper: 0.05,
 });
-
-const setDocHeight = () => {
-  document.documentElement.style.setProperty(
-    '--vh',
-    `${window.innerHeight / 100}px`,
-  );
-};
-
-window.addEventListener('resize', setDocHeight);
-window.addEventListener('orientationchange', setDocHeight);
-
-setDocHeight();
